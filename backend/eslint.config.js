e86af43258141c147593715138e8e13c9ecda2dc
fanual-software-dev <asfawfanual2003@gmail.com>
2025-01-26 22:10:41 +0300

    ESlint changes

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -4,11 +4,24 @@ import prettier from "eslint-config-prettier";
 
 export default [
   js.configs.recommended,
+  prettier,
   {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        process: "readonly",
+        console: "readonly",
+        module: "readonly",
+        require: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        __dirname: "readonly"
+      }
+    },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
+      "no-unused-vars": "warn", // Change unused variables to warning
+      "no-console": "off" // Allow console.log for debugging
     }
-  },
-  prettier
+  }
 ];
